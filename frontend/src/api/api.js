dc1a3f1219a3df47735b0dc6d211c1f9768d349d
Lokesh Sharma <104786100+lokeshkavisth@users.noreply.github.com>
2023-12-31 10:29:22 +0530

    [fix]: Fix base URL access by using VITE_BASE_URL

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const reserveAPI = async ({ method, route, params, data }) => {
-  const BASE_URL = import.meta.env.BASE_URL;
+  const BASE_URL = import.meta.env.VITE_BASE_URL;
   const url = `${BASE_URL}${route}`;
   const options = {
     method,
