e7f98adad390103037f517b8e99c4cd05813f7d4
Lokesh Sharma <104786100+lokeshkavisth@users.noreply.github.com>
2023-12-31 09:49:07 +0530

    [config]: Update CORS allowed origins to enable cross-origin requests

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,11 @@
 require("dotenv").config();
-
 const express = require("express");
 const cors = require("cors");
-
 const connectToMongo = require("./src/config/database");
 const router = require("./src/routes/routes");
 
 const app = express();
-app.use(cors());
-
-// Enable it later
-
-// app.use(
-//   cors({
-//     origin: "http://example.com",
-//   })
-// );
-
+app.use(cors({ origin: "https://reserve-buses.netlify.app/" }));
 app.use(express.json());
 app.use("/", router);
 
@@ -24,6 +13,5 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`The server is running on http://localhost:${PORT}`);
-
   connectToMongo();
 });
