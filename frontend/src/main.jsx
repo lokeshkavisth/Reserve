22e298469a30f7bb68c011e99294299d04ab6be0
Lokesh Sharma <104786100+lokeshkavisth@users.noreply.github.com>
2024-01-02 13:18:06 +0530

    [refactor]: Replace usage of BrowserRouter with HashRouter for improved routing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./styles/index.css";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./config/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
